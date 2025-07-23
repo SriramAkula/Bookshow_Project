c855c16d28f566e7249e1db53ee60db040abb6b2
Sriram Akula <sriramakula212@gmail.com>
2025-07-23 11:13:29 +0530

    Merged server folder from server branch into main

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors=require("cors")
+//.env
+require("dotenv").config();
+//db connection
+require("./src/db");
+
+//Global Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Local Modules
+const { logMiddleware } = require("./src/middlewares");
+const userRouter = require("./src/routers/user.route");
+
+//Middlewares
+app.use(logMiddleware);
+
+app.use(cors({
+    origin: "localhost:3001"
+}))
+
+app.use("/api/users", userRouter);
+// app.use("/movies", moviesRouter);
+// app.use("/bookings", bookingRouter);
+// app.use("/reviews", reviewsRouter);
+
+app.listen(3001, () => console.log("Server is running on 3001 Port!"));
